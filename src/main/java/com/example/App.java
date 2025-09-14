bd177d4d7b637f2ccb604a11830614c7c0859c0f
panku-ksh <pankukshirsagar@gmail.com>
2025-09-14 14:07:32 +0530

    Create App.java

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven! This project is running successfully 🚀");
+    }
+}
